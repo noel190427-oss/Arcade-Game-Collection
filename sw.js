da2a214ea46fe534f4e38921ada30129482ca161
noel190427-oss <noel190427@gmail.com>
2026-09-24 14:19:22 +0200

    Release v0.0.34: Pre-update announcements before upload & live update detection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arcade-collection-v0.0.33-update-notifications';
+const CACHE_NAME = 'arcade-collection-v0.0.34-pre-update-notifications';
 const APP_SHELL = [
   './',
   './index.html',
